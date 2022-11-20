cf2450866db58b370da5af80df8056073152d6f8
Luke V <42780158+PhotoWolf@users.noreply.github.com>
2022-11-19 21:25:42 -0500

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -2,29 +2,22 @@ import torch
 import os
 import glob
 import tqdm
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
-from train import (forward_euler,trainer)
+from train import forward_euler,TrainerConfig
 from dataset import Monosyllabic_Dataset
 from model import ModelConfig
 import pandas as pd
 import tqdm.auto
 
-dataset = Monosyllabic_Dataset('datasets/monosyllabic/df_train.csv',
-                               'datasets/phonetic_features.txt',
-                               'datasets/monosyllabic/sem_train.npz',
-                               sample = False)
+def plot_performance(id):
 
-orthography_tokenizer = dataset.orthography_tokenizer
-phonology_tokenizer = dataset.phonology_tokenizer
+    p2s = np.array(np.load(f'metrics/{id}_eval_p2s_acc.npy'))
+    s2p = np.array(np.load(f'metrics/{id}_eval_s2p_acc.npy')) 
 
-def plot_performance():
-
-    p2s = np.array(torch.load('metrics/phase_1_eval_p2s_acc'))
-    s2p = np.array(torch.load('metrics/phase_1_eval_s2p_acc')) 
-
-    plt.figure(figsize=(15,8),dpi=400)
+    fig = plt.figure(figsize=(15,8),dpi=400)
     plt.subplot(1,2,1)
 
     plt.plot(s2p[:,0])
@@ -48,8 +41,8 @@ def plot_performance():
     
     plt.savefig('figures/phonology_semantics.png')
     
-    o2s = np.array(torch.load('metrics/phase_1_eval_o2s_acc'))
-    o2p = np.array(torch.load('metrics/phase_1_eval_o2p_acc')) 
+    o2s = np.array(np.load(f'metrics/{id}_eval_o2s_acc.npy'))
+    o2p = np.array(np.load(f'metrics/{id}_eval_o2p_acc.npy')) 
 
     plt.figure(figsize=(15,8),dpi=400)
     plt.subplot(1,2,1)
@@ -74,10 +67,11 @@ def plot_performance():
     plt.title('Orthography -> Semantics');
     
     plt.savefig('figures/orthography.png')
+    plt.close(fig)
 
 def plot_taraban(results,filename):
     
-    plt.figure(figsize=(15,8),dpi=400)
+    fig = plt.figure(figsize=(15,8),dpi=400)
     plt.plot([0,1],[np.mean(results[:,2][np.logical_and(results[:,1]==1,results[:,0]==0)]),
                     np.mean(results[:,2][np.logical_and(results[:,1]==1,results[:,0]==1)])],
              label='Regular',color='blue')
@@ -104,6 +98,7 @@ def plot_taraban(results,filename):
     plt.xticks([0,1],['Low Frq','High Frq']);
     
     plt.savefig(f'{filename}_median.png')
+    plt.close(fig)
     
 def eval_taraban(ckpts,model,trainer):
     taraban = pd.read_csv('datasets/monosyllabic/taraban.csv')
@@ -129,17 +124,17 @@ def eval_taraban(ckpts,model,trainer):
                    orthography_tensor = orthography_tokenizer(orthography.iloc[idx])[None]
                    phonology_tensor = phonology_tokenizer(phonology.iloc[idx])[None]
                    
-                   predicted_phonology,_ = trainer(model,{'orthography':orthography})
+                   predicted_phonology,_ = trainer.run(model,{'orthography':orthography_tensor})
                    sse = (phonology_tensor-predicted_phonology[start_step::]).pow(2).sum(dim=(1,2)).mean().item()
                    results.append([frq[idx],con[idx],sse])
                     
-              results = np.array(results)
-              plot_taraban(results,f'figures/taraban/{ckpt}/{start_step}')
+               results = np.array(results)
+               plot_taraban(results,f'figures/taraban/{ckpt}/{start_step}')
              
                 
-def plot_strain(results,filepath):
+def plot_strain(results,filename):
 
-    plt.figure(figsize=(15,8),dpi=400)
+    fig = plt.figure(figsize=(15,8),dpi=400)
     plt.plot([0,1],[np.mean(results[:,3][np.logical_and(np.logical_and(results[:,2]==0,results[:,1]==0),results[:,0]==0)]),
                     np.mean(results[:,3][np.logical_and(np.logical_and(results[:,2]==0,results[:,1]==0),results[:,0]==1)])],
              label='INC-LI',color='blue')
@@ -178,6 +173,7 @@ def plot_strain(results,filepath):
     plt.xticks([0,1],['Low Frq','High Frq']);
     
     plt.savefig(f'{filename}_median.png')
+    plt.close(fig)
     
 def eval_strain(ckpts,model,trainer):
     strain = pd.read_csv('datasets/monosyllabic/strain.csv')
@@ -202,16 +198,16 @@ def eval_strain(ckpts,model,trainer):
                    orthography_tensor = orthography_tokenizer(orthography.iloc[idx])[None]
                    phonology_tensor = phonology_tokenizer(phonology.iloc[idx])[None]
                    
-                   predicted_phonology,_ = trainer(model,{'orthography':orthography})
+                   predicted_phonology,_ = trainer.run(model,{'orthography':orthography_tensor})
                    sse = (phonology_tensor-predicted_phonology[start_step::]).pow(2).sum(dim=(1,2)).mean().item()
                    results.append([frq[idx],con[idx],img[idx],sse])
                     
-              results = np.array(results)
-              plot_strain(results,f'figures/strain/{ckpt}/{start_step}')
+               results = np.array(results)
+               plot_strain(results,f'figures/strain/{ckpt}/{start_step}')
 
     
 def plot_glusko(results,filename):
-    plt.figure(figsize=(15,8),dpi=400)
+    fig = plt.figure(figsize=(15,8),dpi=400)
     plt.plot([0,1],[np.mean(results[:,1][results[:,0]==0]),
                     np.mean(results[:,1][results[:,0]==1])],
              label='Short-Grain',color='blue')
@@ -224,13 +220,13 @@ def plot_glusko(results,filename):
     plt.xticks([0,1],['Ambiguous','Unambiguous']);
     
     plt.savefig(f'{filename}.png')
-    
+    plt.close(fig)    
 
 def eval_glushko(ckpts,model,trainer):
     nonwords = pd.read_csv('datasets/monosyllabic/nonwords.csv')
     orthography = nonwords['ort']
     short_grain = nonwords['pho_small']
-    long_grain = nonwords['pho_long']
+    long_grain = nonwords['pho_large']
     
     cond = nonwords['condition']
     cond = (cond=='unambiguous').astype(float)
@@ -241,14 +237,14 @@ def eval_glushko(ckpts,model,trainer):
        model.eval()
         
        os.makedirs(f'figures/glushko/{ckpt}',exist_ok=True)
-          with torch.no_grad()::
+       with torch.no_grad():
               results = []
               for idx in tqdm.tqdm(range(len(orthography)),position=0):
                    orthography_tensor = orthography_tokenizer(orthography.iloc[idx])[None]
                    short_phonology_tensor = phonology_tokenizer(short_grain.iloc[idx])[None]
                    long_phonology_tensor = phonology_tokenizer(long_grain.iloc[idx])[None]
 
-                   predicted_phonology,_ = trainer(model,{'orthography':orthography})
+                   predicted_phonology,_ = trainer.run(model,{'orthography':orthography_tensor})
                     
                    short_acc = trainer.metrics.compute_phon_accuracy(predicted_phonology,short_phonology_tensor,1)
                    long_acc = trainer.metrics.compute_phon_accuracy(predicted_phonology,long_phonology_tensor,1)
@@ -256,12 +252,12 @@ def eval_glushko(ckpts,model,trainer):
                    results.append([cond[idx],short_acc,long_acc])
 
               results = np.array(results)
-              plot_strain(results,f'figures/glushko/{ckpt}')
+              plot_glusko(results,f'figures/glushko/{ckpt}')
     
 def plot_development(results,filename):
     
-    plt.figure(figsize=(15,8),dpi=400)
-    x = np.linspace(0,4,results[-1].shape[0])
+    fig = plt.figure(figsize=(15,8),dpi=400)
+    x = np.linspace(0,4,results[-1].shape[-1])
     
     plt.plot(x,np.mean(results[0],axis=0),label='P->P',color='blue')
     plt.plot(x,np.mean(results[1],axis=0),label='S->S',color='blue',linestyle=':')
@@ -277,7 +273,8 @@ def plot_development(results,filename):
 
     plt.legend();
     plt.savefig(f'{filename}.png')
-    
+    plt.close(fig)    
+
 def eval_development(ckpts,model,trainer):
     
     global dataset
@@ -289,7 +286,7 @@ def eval_development(ckpts,model,trainer):
        for data in dataset:
            with torch.no_grad():
                values = {'o2s':[],'o2p':[],'s2p':[],'p2p':[],'s2s':[],'p2s':[]}
-               outputs = trainer(model,{'orthography':data['orthography'][None]},delta_t=1/12,return_outputs=True)
+               outputs = trainer.run(model,{'orthography':data['orthography'][None]},delta_t=1/12,return_outputs=True)
             
                values['o2p'].append([torch.sigmoid(output['orth_2_phon']).norm() for output in outputs])
                values['o2s'].append([torch.sigmoid(output['orth_2_sem']).norm() for output in outputs])
@@ -315,22 +312,44 @@ def eval_development(ckpts,model,trainer):
        plot_development(results,f'figures/development/{ckpt}.png')
         
 if __name__=='__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-ID',type=str,default='')
+   args = parser.parse_args()
+
    os.makedirs('figures',exist_ok=True)
+
+
+   dataset = Monosyllabic_Dataset('datasets/monosyllabic/df_train.csv',
+                               'datasets/phonetic_features.txt',
+                               'datasets/monosyllabic/sem_train.npz',
+                               sample = False)
+
+   orthography_tokenizer = dataset.orthography_tokenizer
+   phonology_tokenizer = dataset.phonology_tokenizer
     
-   global dataset
-   phoneme_embeddings = dataset.dataset.phonology_tokenizer.embedding_table.to_numpy()
+   phoneme_embeddings = torch.Tensor(dataset.phonology_tokenizer.embedding_table.to_numpy())
     
    device = torch.device('cpu')
-   trainer = Trainer(forward_euler,phoneme_embeddings,device)
     
-   config = ModelConfig(orth_dim=110,phon_dim=250,sem_dim=2446,learn_bias=False)
-   model = config.create_model().to(device)
+   model_config = ModelConfig(orth_dim=110,phon_dim=250,sem_dim=2446,learn_bias=False)
+   trainer_config = TrainerConfig()
+    
+   model = model_config.create_model().to(device)
+   trainer = trainer_config.create_trainer(phoneme_embeddings).to(device)
    
-   plot_performance()
+   print("Plotting performance...")
+   plot_performance(args.ID)
     
-   ckpts = tqdm.tqdm(glob.glob('ckpts/phase_2*')    
-                     
+   ckpts = glob.glob(f'ckpts/phase_2_{args.ID}*.pth')
+
+   print("Plotting development...")                     
    eval_development(ckpts,model,trainer)
+
+   print("Running Taraban...")                     
    eval_taraban(ckpts,model,trainer)
+
+   print("Running Strain...")                     
    eval_strain(ckpts,model,trainer)
+
+   print("Running Glushko...")                     
    eval_glushko(ckpts,model,trainer)
